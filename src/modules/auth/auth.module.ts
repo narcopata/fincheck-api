774235cfe2e0a60e4ed8fb20b74d221790890408
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-10-10 17:39:39 -0300

    feat: implementando recursos de registro e entrada autenticados

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { DatabaseModule } from 'src/shared/database/database.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { env } from 'src/shared/config/env';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService, JwtService],
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      secret: env.jwtSecret,
+      signOptions: { expiresIn: env.jwtSignExpiresIn },
+    }),
+  ],
+})
+export class AuthModule {}
