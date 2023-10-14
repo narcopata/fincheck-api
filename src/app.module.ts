d1f89a089a796a7fdf64f464a2456047bb690cac
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-10-13 23:36:26 -0300

    feat: adicionando módulo de autenticação à aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { UsersModule } from './modules/users/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule],
   controllers: [],
   providers: [
     {
