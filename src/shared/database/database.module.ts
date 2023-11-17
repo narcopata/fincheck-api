f651b0225d60e0753c16a53d7955a003c0b4b6fd
Rodrigo Silva <rodrigo.walace.silva@gmail.com>
2023-11-17 11:10:08 -0300

    refactor: incluindo repo de transações no módulo do banco

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersRepository } from './repositories/users.repositories';
-import { CategoriesRepository } from './repositories/categories.repositories';
+import { TransactionsRepository } from './repositories/transactions.repositories';
 import { BankAccountsRepository } from './repositories/bank-accounts.repositories';
+import { CategoriesRepository } from './repositories/categories.repositories';
 
 @Module({
   providers: [
     PrismaService,
     UsersRepository,
     CategoriesRepository,
+    TransactionsRepository,
     BankAccountsRepository,
   ],
-  exports: [UsersRepository, CategoriesRepository, BankAccountsRepository],
+  exports: [
+    UsersRepository,
+    CategoriesRepository,
+    TransactionsRepository,
+    BankAccountsRepository,
+  ],
 })
 export class DatabaseModule {}
